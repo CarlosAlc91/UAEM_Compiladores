07ab2b996f687e16320dad98e87cec97d94c01a2
Carlos Manuel Alcantara Rodriguez <carlos.ma.rodriguez91@gmail.com>
2023-06-04 01:41:51 -0600

    function-prototype-exercise-10-7

---
+++ functionPrototyope.c
@@ -0,0 +1,41 @@
+/*
+? EJEMPLO 10.7
+
+* A continuacion se muestra un programa sencillo en C que ilustra la diferencia entre:
+*argumentos ordinarios, que son pasados por valor, y argumentos punteros, que son pasados por referencia.
+ */
+
+#include <stdio.h>
+
+void func1(int u, int v);     /* //prototipo de funcion  */
+void func2(int *pu, int *pv); /* prototipo de una funcion */
+
+int main()
+{
+  int u = 1;
+  int v = 3;
+
+  printf("\nAntes de la llamada a func1: u=%d v=%d", u, v);
+  func1(u, v);
+  printf("\nDespues de la llamada a la func1: u=%d  v=%d", u, v);
+
+  printf("\n\nAntes de la llamada a func2: u=%d v=%d", u, v);
+  func2(&u, &v);
+  printf("\nDespues de la llamada a la func2: u=%d  v=%d", u, v);
+}
+
+void func1(int u, int v)
+{
+  u = 0;
+  v = 0;
+  printf("\nDentro de func1:u       u=%d    v=%d", u, v);
+  return;
+}
+
+void func2(int *pu, int *pv)
+{
+  *pu = 0;
+  *pv = 0;
+  printf("\nDentro de func2:u       *pu=%d    *pv=%d", *pu, *pv);
+  return;
+}
